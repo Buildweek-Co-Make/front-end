3fabca99f6de5e365b80a338562f11f61f94ec10
Avery Burns <a.aburns95@gmail.com>
2020-10-14 20:55:22 -0500

    react app created, dependencies added, basic styles on app.js

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Co-Make App</h1>
+        <p>Make your world the way it should be!</p>
+      </header>
+      <div>
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vitae metus commodo ligula dignissim pretium. In scelerisque, tellus et dapibus mattis, purus diam bibendum orci, non elementum justo ante a urna. Vivamus faucibus dictum turpis sed accumsan. Maecenas aliquet tellus quis pretium aliquam. Ut tempor mauris eget iaculis laoreet. Integer eget pretium ipsum. Nulla sed nulla justo. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed velit nisi, condimentum nec sem non, tincidunt tempus massa.
+
+Suspendisse ac diam at felis maximus aliquam vitae eget augue. Suspendisse vitae vehicula massa, ut rhoncus ex. Mauris vulputate rutrum erat, quis aliquet sapien tristique sit amet. Mauris eget erat venenatis, gravida libero at, consectetur risus. Fusce vel dolor ac turpis ultrices ullamcorper non sit amet purus. Nullam commodo consectetur neque nec malesuada. Integer hendrerit id urna a pellentesque. Nam euismod ex eros. Vivamus varius nibh erat, pretium egestas arcu viverra eu. Cras commodo dolor non rhoncus consectetur. Donec et efficitur magna. Donec euismod, massa eu finibus sollicitudin, erat diam mollis felis, id accumsan leo neque iaculis nunc. Quisque lacinia, arcu eget finibus tincidunt, dolor mauris sodales diam, at iaculis mauris erat vel velit. Suspendisse ultrices sem non justo fermentum, et suscipit nisi vestibulum. Quisque semper cursus leo, id mollis sem sagittis et.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
